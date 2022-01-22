ab9b656e9f58480732d46ffc75cf1b1120ede7f6
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-01-22 17:59:51 +0500

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -26,7 +26,7 @@ export default class Command extends BaseCommand {
 			MessageType.image,
 			{
 				
-				caption: `*A WhatsApp Assistant*\n\nWhich can be use as a group management bot in whatsapp. \nIt have much features send the command *${this.client.config.prefix}help* to know them.\nThis Assistant created with ❤ By Alι_Aryαɴ. \n`,
+				caption: `*I'm Texas*\n\nAn WhatsApp group management bot in whatsapp. \nIt have much features send the command *${this.client.config.prefix}help* to know them.\nThis management bot created with ❤ By Alι_Aryαɴ. \n`,
 			}
 		);
 	};
