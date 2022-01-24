1e5328be6584e2fe0c9f67f7a90a39388727d1ea
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-01-24 23:16:20 +0500

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -29,12 +29,12 @@ export default class Command extends BaseCommand {
 			return void (await M.reply(`Please provide the Broadcast Message.`));
 		const term = joined.trim();
 		const images = [
-			"https://wallpapercave.com/wp/wp3144753.jpg",
-			"https://wallpapercave.com/wp/wp4782018.jpg",
-			"https://wallpaperaccess.com/full/1326836.jpg",
-			"https://wallpapermemory.com/uploads/711/chitoge-kirisaki-wallpaper-full-hd-323316.jpg",
-			"https://data.whicdn.com/images/304776416/original.jpg",
-			"https://i.pinimg.com/564x/ca/e7/8a/cae78ad7f8e6459ad20bde350e2eb78b.jpg",
+			"https://telegra.ph/file/fb37f93ff7526b155cf5f.jpg",
+			"https://telegra.ph/file/fb37f93ff7526b155cf5f.jpg",
+			"https://telegra.ph/file/fb37f93ff7526b155cf5f.jpg",
+			"https://telegra.ph/file/b598703136303565f762a.jpg",
+			"https://telegra.ph/file/b598703136303565f762a.jpg",
+			"https://telegra.ph/file/b598703136303565f762a.jpg",
 		];
 		const selected = images[Math.floor(Math.random() * images.length)];
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
