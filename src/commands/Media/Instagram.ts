9e0e3615ef61341c80aa513e272a813818835a01
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-02-03 23:26:20 +0500

    Update Instagram.ts

--- src/commands/Media/Instagram.ts
+++ src/commands/Media/Instagram.ts
@@ -23,8 +23,7 @@ export default class Command extends BaseCommand {
         if (!joined) return void M.reply('Provide the keywords you wanna search, Baka!')
         const chitoge = joined.trim()
         console.log(chitoge)
-        const { data } = await axios.get(`https://api-xcoders.xyz/api/download/ig?url=${chitoge}&apikey=f8YZwl2SD9`,
-                                         `https://api-xcoders.xyz/api/download/igtv?url=${chitoge}&apikey=f8YZwl2SD9`)
+        const { data } = await axios.get(`https://api-xcoders.xyz/api/download/igtv?url=${chitoge}&apikey=f8YZwl2SD9`)
         if (!data) return void M.reply( await request.buffer(`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEIJBLGeoanLhbUyzTNXLXXRPUDjUuDKIS8g&usqp=CAU`),
         MessageType.image,
                     undefined,
