38b3c220920e2eba2be6a4eb5a6ecdbe2fe1dc22
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-02-05 14:20:46 +0500

    Create Local-Waifu.ts

--- src/commands/Weeb/Local-Waifu.ts
+++ src/commands/Weeb/Local-Waifu.ts
@@ -1,40 +1,42 @@
 /** @format */
 
-import { MessageType } from "@adiwajshing/baileys";
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
-import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import { ISimplifiedMessage } from "../../typings";
 
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
-			command: "lwaifu",
-			description:
-				"Useless",
-			aliases: ["lw", "lf"],
+			command: "localwaifu",
+			description:" useless try by dev",
 			category: "weeb",
-			dm: true,
-			usage: `${client.config.prefix}lwaifu`,
+			usage: `${client.config.prefix}localwaifu`,
+			baseXp: 10,
 		});
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			"https://telegra.ph/file/fb37f93ff7526b155cf5f.jpg",
-			"https://telegra.ph/file/fb37f93ff7526b155cf5f.jpg",
-			"https://telegra.ph/file/fb37f93ff7526b155cf5f.jpg",
-			"https://telegra.ph/file/b598703136303565f762a.jpg",
-			"https://telegra.ph/file/b598703136303565f762a.jpg",
-			"https://telegra.ph/file/b598703136303565f762a.jpg",
-		];
+			"https://cdn.discordapp.com/attachments/688526480069885997/688542864904683599/AmistaAnime.jpg",
+                        "https://cdn.discordapp.com/attachments/688526480069885997/688543620005101605/AmistaAnime.jpg",
+                        "https://cdn.discordapp.com/attachments/688526480069885997/688546517102886912/AmistaAnime.jpg",
+                        "https://cdn.discordapp.com/attachments/688526480069885997/688546519829315623/AmistaAnime.jpg",
+                        "https://cdn.discordapp.com/attachments/688526480069885997/688547270437765134/AmistaAnime.jpg",
+                        "https://cdn.discordapp.com/attachments/688526480069885997/688554574851735732/AmistaAnime.jpg",
+                        "https://cdn.discordapp.com/attachments/688526480069885997/688558096943153261/AmistaAnime.jpg",
+                        "https://cdn.discordapp.com/attachments/688526480069885997/688565021038346250/AmistaAnime.jpg",
+                        "https://cdn.discordapp.com/attachments/688526480069885997/688566273897922591/AmistaAnime.jpg",
+                        "https://cdn.discordapp.com/attachments/688526480069885997/688571436591349780/AmistaAnime.jpg",
+;
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
 			MessageType.image,
 			{
 				
-				caption: `Useless try By Alι_.\n`,
+				caption: `Useless feature By Alι_.\n`,
 			}
 		);
 	};
