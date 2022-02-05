7922cea68df801d1fffba1647e576cdc08492781
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-02-05 23:43:12 +0500

    Update Everyone.ts

--- src/commands/Moderation/Everyone.ts
+++ src/commands/Moderation/Everyone.ts
@@ -15,7 +15,8 @@ export default class Command extends BaseCommand {
 			aliases: ["all", "tagall", "ping"],
 			category: "moderation",
 			usage: `${client.config.prefix}everyone`,
-			adminOnly: false,
+			adminOnly: true,
+                        modsOnly: true,
 			baseXp: 20,
 		});
 	}
