b3fb72e158825c6e255301f5f2d0d940a1b69cc7
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-03-19 00:49:53 +0500

    Update Name-Hub.ts

--- src/commands/Utils/Name-Hub.ts
+++ src/commands/Utils/Name-Hub.ts
@@ -1,59 +1,53 @@
+import { MessageType } from '@adiwajshing/baileys'
+import request from '../../lib/request'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { IParsedArgs, ISimplifiedMessage } from '../../typings'
-import akaneko from "akaneko";
-import request from "../../lib/request";
-import { MessageType } from "@adiwajshing/baileys";
-// import { MessageType, Mimetype } from '@adiwajshing/baileys'
-const w5botapi = require('w5-textmaker');
+import axios from 'axios'
 
 export default class Command extends BaseCommand {
-	constructor(client: WAClient, handler: MessageHandler) {
-		super(client, handler, {
-			command: "namehub",
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: "namehub",
 			description: `Get text image of HUB`,
 			aliases: ["nhub", "nh", "hub"],
 			category: "utils",
 			usage: `${client.config.prefix}namehub`,
 			baseXp: 50,
-		});
-	}
-
-	run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-		 if (!joined) return void (await M.reply(`Provide the text, Baka!`))
-        const cara = joined.trim()
-		const wall = await w5botapi.textpro("https://api-xcoders.xyz/api/textpro/pornhub?text=${chitoge}&text2=hub&apikey=tFnG6PJvrg",
-    cara
-    );
-		const buffer = await request.buffer(wall).catch((e) => {
-			return void M.reply(e.message);
-		});
-		while (true) {
-			try {
-				M.reply(
-					buffer || "Could not fetch image. Please try again later",
-					MessageType.image,
-					undefined,
-					undefined,
-					`🌟 Here you go.\n`,
-					undefined
-				).catch((e) => {
-					console.log(
-						`This Error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`
-					);
-					// console.log('Failed')
-					M.reply(`Could not fetch image. Here's the URL: ${wall}`);
-				});
-				break;
-			} catch (e) {
-				// console.log('Failed2')
-				M.reply(`Could not fetch image. Here's the URL : ${wall}`);
-				console.log(
-					`This Error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`
-				);
-			}
-		}
-		return void null;
-	};
+        })
+    }
+    // static count = 0
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        
+        if (!joined) return void M.reply('Provide the keywords you wanna search, Baka!')
+        const chitoge = joined.trim()
+        console.log(chitoge)
+        const { data } = await axios.get(`https://api-xcoders.xyz/api/textpro/pornhub?text=${chitoge}&text2=hub&apikey=tFnG6PJvrg`)
+        const buffer = await request.buffer(data.url).catch((e) => {
+            return void M.reply(e.message)
+        })
+        while (true) {
+            try {
+                M.reply(
+                    buffer || 'Could not fetch image. Please try again later',
+                    MessageType.image,
+                    undefined,
+                    undefined,
+                    `Here you go ✨\n`,
+                    undefined
+                ).catch((e) => {
+                    console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
+                    // console.log('Failed')
+                    M.reply(`Could not fetch image. Here's the URL: ${data.url}`)
+                })
+                break
+            } catch (e) {
+                // console.log('Failed2')
+                M.reply(`Could not fetch image. Here's the URL : ${data.url}`)
+                console.log(`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
+            }
+        }
+        return void null
+    }
 }
