5898e9563503bf25456010aeda4afb38cb40af41
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-03-21 21:29:09 +0500

    Update and rename Chitoge.ts to Texas.ts

--- src/commands/General/Texas.ts
+++ src/commands/General/Texas.ts
@@ -12,22 +12,21 @@ export default class Command extends BaseCommand {
 			command: "texas",
 			description: "Displays the info",
 			category: "general",
-			usage: `${client.config.prefix}chitoge`,
+			usage: `${client.config.prefix}texas`,
 			baseXp: 200,
 		});
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			"https://telegra.ph/file/7b2e3b0e08229c111311f.mp4";
+			"https://telegra.ph/file/1c0170c930d5ab65bbcad.jpg";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
-			MessageType.video,
+			MessageType.image,
 			{
-				quoted: M.WAMessage,
-				mimetype: Mimetype.gif,
-				caption: ` *I'm Texαs* ❤ \n\n*Description:*\nA WhatsApp bot with a lot anime features! \n*Thanks To Alι_*\n`,
+				
+				caption: ` *I'm Texαs* ❤ \n\n*Description:*\nA WhatsApp bot with a lot anime features! Use ${client.config.prefix}support to join our WhatsApp Support Group.\n*Thanks To Alι_*\n`,
 			}
 		);
 	};
