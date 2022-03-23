1dad53de9453e2235732341460b1704d4eb9b82d
Sungmin07 <93863844+Sungmin07@users.noreply.github.com>
2022-03-23 21:59:53 +0530

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			"https://c.tenor.com/ldlrOa43OlMAAAPo/majo-no-tabitabi-the-journey-of-elaina.mp4";
+			"https://c.tenor.com/9V4q8VYYFTUAAAPo/wink-blink.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `Hello there, Elaina Here 💕. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
+				caption: `Hello there, Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
 			}
 		);
 	};
