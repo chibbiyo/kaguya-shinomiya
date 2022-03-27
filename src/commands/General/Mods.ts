8debdd2120586d69c53e1d0f69a21ca44afc24ef
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-03-27 16:48:44 +0500

    Die

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -18,14 +18,14 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '*Moderators List* 👮\n\n'
+        let text = '🥷 *Moderators*\n\n'
         filteredMap.forEach(
             (user, index) =>
-                (text += `#${index + 1}\n👨‍💻 *Username: ${
+                (text += `#${index + 1}\n🧑‍💻 *Username: ${
                     user.notify || user.vname || user.name || 'null'
-                }*\n*Contact Info: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
+                }*\n*Contact info: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n*Texαs* ✨`
+        text += `\n*Texαs* ✨ `
         return void M.reply(text)
     }
 }
