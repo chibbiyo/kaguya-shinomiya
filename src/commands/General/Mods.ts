f024089b7c576b34c459b227261e8837a475933d
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-03-27 16:54:11 +0500

    Hmm

---
+++ src/commands/General/Mods.ts
@@ -0,0 +1,30 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'mods',
+            description: "Displays the Moderators' contact info",
+            category: 'general',
+            usage: `${client.config.prefix}mods`,
+            aliases: ['moderators', 'mod', 'owner']
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
+        const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
+        let text = '*Moderators List* 👮\n\n'
+        filteredMap.forEach(
+            (user, index) =>
+                (text += `#${index + 1}\n👨‍💻 *Username: ${
+                    user.notify || user.vname || user.name || 'null'
+                }*\n*Contact Info [Hidden by Alι_]*\n\n`) /* Use this code to show the Contact Numbers: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)*/
+        )
+        text += `\nContact Alι_Aryαɴ at Telegram! \nhttps://t.me/AliAryanTech/ \n\n• *Texαs* ✨`
+        return void M.reply(text)
+    }
+}
