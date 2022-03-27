2e944e3aaa559bb306a97641244d727575493736
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-03-28 00:21:53 +0500

    Nice

--- src/commands/Dev/Ban.ts
+++ src/commands/Dev/Ban.ts
@@ -9,6 +9,7 @@ export default class Command extends BaseCommand {
             command: 'ban',
             description: 'Bans the tagged users globally',
             category: 'dev',
+            dm: true,
             usage: `${client.config.prefix}ban [@tag]`,
             modsOnly: true,
             baseXp: 0
