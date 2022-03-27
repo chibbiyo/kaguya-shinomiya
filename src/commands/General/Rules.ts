10339fb75ddef00877f5798c151ec41a7c99b840
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-03-27 16:27:13 +0500

    Ok

--- src/commands/General/Rules.ts
+++ src/commands/General/Rules.ts
@@ -9,10 +9,10 @@ import { ISimplifiedMessage } from "../../typings";
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
-			command: "ra",
-			description: "Gp",
+			command: "rules",
+			description: "This Command with display the bot rules.",
 			category: "general",
-			usage: `${client.config.prefix}ra`,
+			usage: `${client.config.prefix}rules`,
                         dm: true,
 			baseXp: 10,
 		});
@@ -21,7 +21,13 @@ export default class Command extends BaseCommand {
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		
 		await M.reply(
-			`*━━❰Texαs❱━━*`
+			`🤖 *ᴛᴇxᴀs ʙᴏᴛ ʀᴜʟᴇs*\n
+                         📍 _Do not call bot, if you do then you'll get automatically blocked by bot._\n
+                         📍 _Do not spam bot with commands, if bot is not responding then it means either bot is off or there's internet issue at owner's end._\n
+                         📍 _Do not abuse/disrespect bot and its owner._\n
+                         📍 _If you see any bug/error in bot then report it to owner with the command !bug/report <problem>._\n
+                         📍 _If you want this bot in your group then contact owner in support group by typing !support or use !report command as request with group join link._\n
+                         📍 _Enjoy the bot and have fun._\n`
 		);
 	};
 }
