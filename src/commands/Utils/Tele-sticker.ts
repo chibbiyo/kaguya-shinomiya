2cc87a54a204fe5afdf63d881e2cb5edb0cf8e3c
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-03-28 09:26:07 +0500

    Hmm

--- src/commands/Utils/Tele-sticker.ts
+++ src/commands/Utils/Tele-sticker.ts
@@ -19,42 +19,30 @@ export default class Command extends BaseCommand {
         })
     }
     // static count = 0
-     run = async (
-		M: ISimplifiedMessage,
-		{ joined }: IParsedArgs
-	): Promise<void> => {
-		if (!joined)
-			return void (await M.reply(`Give me the download url, Baka!`));
-		const texas: any = joined.trim().split("|");
-		const term: string = chitoge[0];
-		const amount: number = chitoge[1];
-		if (!amount)
-			return void M.reply(
-				`Give me the number , Baka!\n\nExample: *${this.client.config.prefix}telesticker url|5*`
-			);
-		if (amount > 20)
-			return void M.reply(`Do you want me to spam in this group?`);
-                        const texas = joined.trim()
-                        console.log(texas)
-                          const { data } = await axios.get(`https://zenzapi.xyz/downloader/telesticker?apikey=867fe8d278&url=${texas}`)
+     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
         
-    if ((data as { error: string }).error) return void (await M.reply('Sorry, couldn\'t find'))
+        if (!joined) return void M.reply('Provide the keywords you wanna search, Baka!')
+        const texas = joined.trim()
+        console.log(texas)
+        
+        const { data } = await axios.get(`https://zenzapi.xyz/downloader/telesticker?apikey=867fe8d278&url=${texas}`)
+        
+if ((data as { error: string }).error) return void (await M.reply('Sorry, couldn\'t find'))
         //const i = Math.floor(Math.random() * data.result.length)
-    const b = `${data.results?.[Math.floor(Math.random() * data.results.length)]?.media[0]?.mp4?.url}`
+const b = `${data.results?.[Math.floor(Math.random() * data.results.length)]?.media[0]?.webp?.url}`
 
-    for (let i = 0; i < amount; i++) {
-const sticker: any = await new Sticker(b, {
+        const sticker: any = await new Sticker(b, {
 			pack: "Sticker Pack",
 			author: "BY Texαs ✨",
 			quality: 90,
 			type: "crop",
 			categories: ["🎊"],
 		});
-await M.reply(
+
+      await M.reply(
 			await sticker.build(),
 			MessageType.sticker,
 			Mimetype.webp,)
 }
+
 }
-
-
