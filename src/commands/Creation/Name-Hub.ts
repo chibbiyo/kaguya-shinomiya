fdf2ecdfb62f3c0e9a1fec6e2f999fbe7be4a756
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-03-28 21:31:48 +0500

    Ok

---
+++ src/commands/Creation/Name-Hub.ts
@@ -0,0 +1,33 @@
+import { MessageType } from '@adiwajshing/baileys'
+import request from '../../lib/request'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: "namehub",
+			description: `Get text image of HUB`,
+			aliases: ["nhub", "nh", "hub"],
+			category: "utils",
+			usage: `${client.config.prefix}namehub`,
+			baseXp: 50,
+        })
+    }
+    // static count = 0
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        if (!joined) return void (await M.reply(`Give me your name one word only first or last one, Baka!`))
+        const chitoge = joined.trim()
+        return void M.reply( await request.buffer(`https://api-xcoders.xyz/api/textpro/pornhub?text=${chitoge}&text2=hub&apikey=tFnG6PJvrg`),
+        MessageType.image,
+                    undefined,
+                    undefined,
+                    `✨ Here you go.\n`,
+                    undefined
+                ).catch((reason: any) =>
+            M.reply(`✖ An error occurred. Please try again later. ${reason}`))
+    }
+}
