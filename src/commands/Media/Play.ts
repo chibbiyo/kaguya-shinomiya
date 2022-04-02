872e79db6eb7c1d6791a8c4cc75b8419a64f7c7d
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-04-02 12:33:00 +0500

    Update Play.ts

--- src/commands/Media/Play.ts
+++ src/commands/Media/Play.ts
@@ -42,7 +42,7 @@ export default class Command extends BaseCommand {
             body: `Author : ${videos[0].author.name.substr(
               0,
               20
-            )}\n✨ Texαs ✨`,
+            )}\nTexαs ✨`,
             mediaType: 2,
             thumbnail: await this.client.getBuffer(
               `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`
