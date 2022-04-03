32d4f67377cd48572ffa193874eb3d691b8f0b4e
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-04-03 13:36:42 +0500

    Ignore

--- src/commands/Media/Play.ts
+++ src/commands/Media/Play.ts
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
       );
     const audio = new YT(videos[0].url, "audio");
     if (!audio.url) return;
-    M.reply("✨ 𝙎𝙚𝙣𝙙𝙞𝙣𝙜..");
+    M.reply("✨ 𝙎𝙚𝙣𝙙𝙞𝙣𝙜...");
     this.client
       .sendMessage(M.from, await audio.getBuffer(), MessageType.audio, {
         quoted: M.WAMessage,
