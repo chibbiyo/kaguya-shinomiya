4dddec6b341bd038e40f04ed51b700025843e033
chibbiyo <103373807+chibbiyo@users.noreply.github.com>
2022-05-17 14:00:01 +0100

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -48,7 +48,7 @@ export default class Command extends BaseCommand {
 │⋊ ℕ𝕒𝕞𝕖: Sʜɪɴᴏᴍɪʏᴀ
 │⋊ ℙ𝕣𝕖𝕗𝕚𝕩: ${this.client.config.prefix}
 │⋊ 𝕆𝕨𝕟𝕖𝕣: *${this.client.config.prefix}mod*
-│⋊ 𝕆𝕗𝕗𝕚𝕔𝕚𝕒𝕝 𝔾𝕣𝕠𝕦𝕡: https://chat.whatsapp.com/G89MF8YbNPt2zOdF6HItyD
+│⋊ 𝕆𝕗𝕗𝕚𝕔𝕚𝕒𝕝 𝔾𝕣𝕠𝕦𝕡: http://gg.gg/shinomiya-support
 ╰────────────┈平和                            \n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
