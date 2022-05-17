6008e77d00b668946ce28eea940f4d21f9286fd6
chibbiyo <103373807+chibbiyo@users.noreply.github.com>
2022-05-17 14:20:16 +0100

    Update Rules.ts

--- src/commands/General/Rules.ts
+++ src/commands/General/Rules.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
         let chitoge = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
             mimetype: Mimetype.gif,
-            caption: `_*----🎀[Rule]🎀----*_\n\n❌ *DONT ASK FOR THE SCRIPT* 🚫\n*>>>* use .support to get the Official group link in your dm\n*--->Don't chat in private bot can't chat in Pm\n*--->* If you want to add shinomiya in your group then contact the owner by *.owner/.mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫  ` }
+            caption: `_*----🎀[Rules]🎀----*_\n\n❌ *DO NOT CALL THE BOT* 🚫\n*>>>* use /support to get the support group link in your dm\n*--->* Don't chat in private bot can't chat in Pm\n*--->* If you want to add shinomiya in your group then contact the mods by */mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BANNED* 🚫  ` }
         )
     }
 }
